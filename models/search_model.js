685a175ce0bec514161675f0d2c750eed261655e
Luis Lithgow <lalithgow@gmail.com>
2016-07-20 19:32:21 -0400

    Set up search routes and search model

--- models/search_model.js
+++ models/search_model.js
@@ -5,13 +5,31 @@ const pg = require('pg-promise')({
 });
 
 const config = {
-host:       process.env.DB_HOST,
-port:       process.env.DB_PORT,
-database:   process.env.DB_NAME,
-user:       process.env.DB_USER,
-password:   process.env.DB_PASS,
+  host:       process.env.DB_HOST,
+  port:       process.env.DB_PORT,
+  database:   process.env.DB_NAME,
+  user:       process.env.DB_USER,
+  password:   process.env.DB_PASS,
 };
 
 const _db = pg(config);
 
-module.exports = _db;
+module.exports = {
+
+
+  // GET all users and their interests and skills
+  getUserTypes(req,res,next) {
+    _db.any(`SELECT users.display_name, attributes.attr_name, attributes.attr_type FROM users INNER JOIN user2attribute as u2a on users.user_id = u2a.user_id INNER JOIN attributes on u2a.attribute_id = attributes.attribute_id; ` )
+        .then( types=>{
+          // res.rows = types ;
+          console.log("Succes at getting user skills or interests!");
+          next()
+        })
+        .catch( error=> {
+          console.error('You have a getUserTypes error!! ', error);
+          throw error;
+        })
+  }
+
+
+}
--- routes/search_rt.js
+++ routes/search_rt.js
@@ -1,8 +1,16 @@
 const searchRouter = require('express').Router();
+const db           = require('../models/search_model');
+
+
+/* convenience method for sending */
+const sendJSONresp = (req,res)=>res.json(res.rows)
+
+
 
 searchRouter.route('/')
-  .get((req, res)=>{
-     res.send('you hit the Search Route')
-  })
+      .get(db.getUserTypes, sendJSONresp)
+// searchRouter.route('/search/interest/:id')
+//       .get(db.getUserTypes, sendJSONresp)
+
 
 module.exports = searchRouter;
