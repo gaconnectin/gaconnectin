03e32169177dae7b6e7619e0c451a5fa3f6bce88
Heidi Powers <heidipowers@gmail.com>
2016-07-22 17:27:35 -0400

    Working on Search db connection

--- src/client/app/SearchOption.jsx
+++ src/client/app/SearchOption.jsx
@@ -2,27 +2,54 @@ import React from 'react';
 
 
 export default function SearchOption(props){
-
-  return(
+  console.log(props.userChoice)
+  if(props.userChoice == 'skills'){
+    return(
 
           <div className="row text-center">
             <div>
               <form>
+
               <h2>I want to find someone who likes</h2>
                  <select>
-                    {props.userChoice.map(function(item, index){
+                    {props.userSkill.map(function(item, index){
                       return <option key={index}>{item}</option>
                     })}
 
                  </select>
 
-                <input name="interest" type="text" placeholder="enter an interest here"/>
                 <button className="btn btn-default" type="submit">FIND!</button>
               </form>
             </div>
             <hr />
           </div>
     )
+
+  } else {
+
+    return (
+        <div className="row text-center">
+                <div>
+                  <form>
+
+                  <h2>ATTR</h2>
+
+
+                    <input name="interest" type="text" placeholder="enter an interest here"/>
+                    <button className="btn btn-default" type="submit">FIND!</button>
+                  </form>
+                </div>
+                <hr />
+              </div>
+
+
+      )
+
+
+
+  }
+
+
 
 
 }
